f98d7b0c7b1db831da28e40a169b0ad19c72d5fd
Bhooshan <me.45.birjebhooshan@gmail.com>
2023-03-23 12:26:42 +0530

    use cors

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
+const cors = require("cors")
 const pinRoute = require("./routes/pins")
 const userRoute = require("./routes/users")
 
 dotenv.config();
 
+app.use(cors())
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL).then(()=>{
